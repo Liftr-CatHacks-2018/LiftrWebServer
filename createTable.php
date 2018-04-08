9736e5dd523dc4319a7b244a12036962b8a14049
Andrew Combs <ajcomb01@louisville.edu>
2018-04-07 22:39:06 -0400

    Add a file to test database connection

---
+++ createTable.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "liftrdb-ch18.cdviygl7ogql.us-east-2.rds.amazonaws.com";
+$username = "liftrAdmin";
+$password = "Vw5MxF2X6vvJ";
+$dbname = "LiftData";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
